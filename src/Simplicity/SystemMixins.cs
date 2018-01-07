12586a68be9548121fca9bec2772668af3a26ff7
Unknown <jonstodle@outlook.com>
2018-01-07 22:12:54 +0100

    Add T.Dump()

--- src/Simplicity/SystemMixins.cs
+++ src/Simplicity/SystemMixins.cs
@@ -13,5 +13,28 @@ namespace System
         /// <param name="minimumLength">The minimum required length</param>
         /// <returns>Returns <c>true</c> if the string is not null and is at least the minimum specified length, else <c>false</c></returns>
         public static bool HasValue(this string source, int minimumLength = 1) => !string.IsNullOrWhiteSpace(source) && source.Length >= minimumLength;
+
+        /// <summary>
+        /// Write to the Debug output and return the original object unmodified
+        /// </summary>
+        /// <param name="source">The object to write to the Debug output</param>
+        /// <param name="selector">A <c>Func</c> that selects the property to be written</param>
+        /// <param name="prefix">A prefix to write before the output. Use this to easily identify individual <c>Dump</c>s</param>
+        /// <typeparam name="T">The type of the object to write to the Debug output</typeparam>
+        /// <returns>The original object</returns>
+        public static T Dump<T>(this T source, Func<T, object> selector, string prefix = "")
+        {
+            System.Diagnostics.Debug.WriteLine($"{prefix}{(prefix.HasValue() ? ": " : "")}{selector(source)}");
+            return source;
+        }
+
+        /// <summary>
+        /// Write to the Debug output and return the original object unmodified
+        /// </summary>
+        /// <param name="source">The object to write to the Debug output</param>
+        /// <param name="prefix">A prefix to write before the output. Use this to easily identify individual <c>Dump</c>s</param>
+        /// <typeparam name="T">The type of the object to write to the Debug output</typeparam>
+        /// <returns>The original object</returns>
+        public static T Dump<T>(this T source, string prefix = "") => source.Dump(value => value, prefix);
     }
 }
