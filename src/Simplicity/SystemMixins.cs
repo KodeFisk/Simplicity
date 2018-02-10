7fc5d5da45ffa9fe8d3b85c2ea27b85a9f85466c
Unknown <jonstodle@outlook.com>
2018-02-10 18:59:32 +0100

    Change parameter to This

--- src/Simplicity/SystemMixins.cs
+++ src/Simplicity/SystemMixins.cs
@@ -9,9 +9,9 @@ namespace System
         /// <summary>
         /// Checks if the string has a value of at least the minimum length
         /// </summary>
-        /// <param name="source">The <c>string</c> to check</param>
+        /// <param name="This">The <c>string</c> to check</param>
         /// <param name="minimumLength">The minimum required length</param>
         /// <returns>Returns <c>true</c> if the string is not null and is at least the minimum specified length, else <c>false</c></returns>
-        public static bool HasValue(this string source, int minimumLength = 1) => !string.IsNullOrWhiteSpace(source) && source.Length >= minimumLength;
+        public static bool HasValue(this string This, int minimumLength = 1) => !string.IsNullOrWhiteSpace(This) && This.Length >= minimumLength;
     }
 }
