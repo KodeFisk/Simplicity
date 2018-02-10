d7753438e5707dfec7ca328c580c062bb37ba723
Unknown <jonstodle@outlook.com>
2018-02-10 19:04:23 +0100

    Add punctuations to the end of the sentences

--- src/Simplicity/SystemMixins.cs
+++ src/Simplicity/SystemMixins.cs
@@ -7,11 +7,11 @@ namespace System
     public static class SystemMixins
     {
         /// <summary>
-        /// Checks if the string has a value of at least the minimum length
+        /// Checks if the string has a value of at least the minimum lengt.
         /// </summary>
-        /// <param name="This">The <c>string</c> to check</param>
-        /// <param name="minimumLength">The minimum required length</param>
-        /// <returns>Returns <c>true</c> if the string is not null and is at least the minimum specified length, else <c>false</c></returns>
+        /// <param name="This">The <c>string</c> to check.</param>
+        /// <param name="minimumLength">The minimum required length.</param>
+        /// <returns>Returns <c>true</c> if the string is not null and is at least the minimum specified length, else <c>false</c>.</returns>
         public static bool HasValue(this string This, int minimumLength = 1) => !string.IsNullOrWhiteSpace(This) && This.Length >= minimumLength;
     }
 }
